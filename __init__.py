be9243937a5de3da1d41c568403ca2a380eaf2c7
jtroo <j.e.troost@student.vu.nl>
2022-02-03 23:50:29 +0100

    debug message

--- __init__.py
+++ __init__.py
@@ -5,9 +5,13 @@ class SonosSpeaker(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
+    def initialize(self):
+        self.add_event('speak', self.handle_speaker_sonos)
+
     @intent_file_handler('speaker.sonos.intent')
     def handle_speaker_sonos(self, message):
-        self.speak_dialog('speaker.sonos')
+        # self.speak_dialog('speaker.sonos')
+        self.log.debug(message)
 
 
 def create_skill():
